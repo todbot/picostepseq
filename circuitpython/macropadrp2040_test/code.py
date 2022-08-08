625be1887e999244c83fee57b124aa253d9b6590
Tod Kurt <todbot@users.noreply.github.com>
2022-08-07 18:57:17 -0700

    test code on macropad

---
+++ circuitpython/macropadrp2040_test/code.py
@@ -0,0 +1,197 @@
+# macropadrp2040_step_sequencer_test.py -- 
+# 6 Aug 2022 - @todbot / Tod Kurt
+#
+# User interface:
+# - turn encoder to change tempo
+# - tap encoder to toggle play / pause
+#
+
+
+# built in modules
+import random
+import board, keypad
+import displayio, terminalio
+
+# installed via circup
+import adafruit_midi
+import adafruit_macropad
+from adafruit_display_text import bitmap_label as label
+
+# local libraries in CIRCUITPY
+from step_sequencer import StepSequencer, ticks_ms, ticks_diff
+#from adafruit_ticks import ticks_ms, ticks_add, ticks_less
+
+printdebug = True
+
+base_note = 48  #  C3
+num_steps = 8
+tempo = 10
+gate_default = 0    # ranges 0-15
+
+macropad = adafruit_macropad.MacroPad()
+macropad.pixels.brightness = 0.2
+macropad._encoder_switch.deinit()
+encoder_switch = keypad.Keys((board.BUTTON,), value_when_pressed=False, pull=True)
+
+maingroup = displayio.Group()
+macropad.display.show(maingroup)
+
+step_to_key_pos = (1, 4, 7, 10, 0, 3, 6, 9)
+
+#step_text_pos = ( (45,10), (45,20), (45,30), (45,40),
+#                  (5,10), (5,20), (5,30), (5,40) )
+step_text_pos = ( (0,10), (30,10), (60,10), (90,10),
+                  (0,25), (30,25), (60,25), (90,25) )
+                  
+stepgroup = displayio.Group()
+maingroup.append(stepgroup)
+for (x,y) in step_text_pos:
+    stepgroup.append( label.Label(terminalio.FONT, text="txt ", x=x, y=y))
+tempo_text = label.Label(terminalio.FONT, text="tmpo", x=0, y=57)
+play_text = label.Label(terminalio.FONT, text="play", x=100, y=57)
+transpose_text = label.Label(terminalio.FONT, text="trans", x=50, y=57)
+maingroup.append(tempo_text)
+maingroup.append(play_text)
+maingroup.append(transpose_text)
+
+
+play_note_last = 0   #FIXME need gate time
+
+def play_note(step, note, vel, gate, on):  # 
+    global play_note_last
+    macropad.pixels.fill(0)
+    macropad.pixels[step_to_key_pos[step]] = 0xff0000 if on else 0x888888  # FIXME: what to do for step switches?
+    macropad.midi.send( macropad.NoteOff(play_note_last,0), channel=0)
+    if on:
+        if printdebug: print("play:%d n:%3d v:%3d %d %d" % (step, note,vel, gate,on), end="\n" )
+        macropad.midi.send( macropad.NoteOn(note, vel), channel=0)
+        play_note_last = note
+
+def update_disp_step(i, n, v=127, gate=8, on=True):
+    print("udpate_disp_step:", i,n,v,gate,on )
+    nstr = seq.notenum_to_name(n) # if on else '---'
+    gstr = " " if on else '*'
+    stepgroup[i].text = "%1s%3s%1s" % (" ", nstr, gstr)
+
+def update_disp_tempo():
+    tempo_text.text = str(tempo)
+
+def update_disp_playing():
+    play_text.text = " > " if seq.playing else "|| "
+
+def update_disp_transpose():
+    transpose_text.text = "%+2d" % seq.transpose 
+
+def update_display():
+    update_disp_tempo()
+    update_disp_playing()
+    update_disp_transpose()
+    for i in range(num_steps):
+        (n,v,gate,on) = seq.steps[i]
+        update_disp_step( i, n, v, gate, on)
+
+
+seq = StepSequencer(num_steps, tempo, play_note)
+
+last_debug_millis = 0
+last_encoder_val = macropad.encoder
+encoder_push_millis = 0  # when was encoder pushed
+encoder_delta = 0
+step_push = -1
+step_push_millis = 0   # when was a step button pushed
+
+# init sequencer with saved / default
+for i in range(num_steps):
+    (n,v,gate,on) = (base_note, 127, gate_default, True)
+    seq.steps[ i ] = (n,v,gate,on)
+    update_disp_step(i, n, v, gate, on)
+# init display
+update_display()
+
+
+while True:
+    
+    seq.update()
+
+    #if ticks_diff(ticks_ms(), last_debug_millis) > 5000:
+    #    last_debug_millis = ticks_ms()
+    #    print(last_debug_millis, "----------- hi")
+
+    # update encoder turning
+    encoder_val = macropad.encoder
+    if encoder_val != last_encoder_val:
+        encoder_delta = (encoder_val - last_encoder_val)
+        last_encoder_val = encoder_val
+        
+    # if encoder turned
+    if encoder_delta:
+        # encoder turned while step key held == change step's note
+        if step_push > -1:  # step key pressed
+            (n,v,gate,on) = seq.steps[ step_push ]
+            n = n + encoder_delta
+            seq.steps[ step_push ] = (n,v,gate,on)
+            update_disp_step( step_push, n, v, gate, on)
+
+        # encoder turned while encoder pushed == change transpose
+        elif encoder_push_millis > 0:
+            seq.transpose += encoder_delta
+            update_disp_transpose()
+        
+        # encoder turned without any modifiers == change tempo
+        else:
+            tempo = tempo + encoder_delta
+            seq.set_tempo(tempo)
+            update_disp_tempo()
+
+        encoder_delta = 0  # we used up the encoder
+
+    # encoder push
+    encsw = encoder_switch.events.get()
+    if encsw:
+        if encsw.pressed:
+            encoder_push_millis = ticks_ms()  # save when we pushed encoder
+            
+            # encoder push while holding step key == mute/unmute
+            if step_push > -1:
+                (n,v,gate,on) = seq.steps[step_push]
+                on = not on  # toggle
+                seq.steps[ step_push ] = (n,v,gate,on)
+                update_disp_step( step_push, n, v, gate, on )
+
+        if encsw.released:
+            # encoder tap, with no key == play/pause
+            if ticks_diff( ticks_ms(), encoder_push_millis) < 300 and step_push == -1:
+                seq.playing = not seq.playing
+                update_disp_playing()
+            # # encoder hold with no key == STOP and reset playhead to 0
+            # FIXME: broken. doesn't re-start at 0 properly
+            # elif ticks_diff( ticks_ms(), encoder_push_millis) > 1000 and step_push == -1:
+            #     seq.stop()
+            #     update_display()
+            
+            encoder_push_millis = 0
+    
+    # step key press
+    key = macropad.keys.events.get()
+    if key:
+        if key.pressed:
+            print("press",key.key_number) #, macropad.encoder, macropad.encoder_switch)
+            try:
+                # record which step key is pushed for other UI modifiers 
+                step_push = step_to_key_pos.index(key.key_number) # map key pos back to step num
+                step_push_millis = ticks_ms()
+                # if not playing, step keys play their pitches
+                if not seq.playing:
+                    (n,v,on) = seq.steps[step_push]
+                    play_note( step_push, n, v, True ) # note on
+            except ValueError:
+                pass
+                
+        if key.released:
+            print("release", key.key_number)
+            #if ticks_diff( ticks_ms(), step_push_millis)
+            step_push = -1
+            # if not playing, step keys play their pitches
+            if not seq.playing:
+                (n,v,on) = seq.steps[step_push]
+                play_note( step_push, n, 0, True ) # note off = note on w/ zero vel
---
+++ circuitpython/macropadrp2040_test/step_sequencer.py
@@ -0,0 +1,96 @@
+
+from adafruit_ticks import ticks_ms, ticks_diff
+
+# let's get a millis function
+# try:
+#     from supervisor import ticks_ms  # thank you dhalbert
+# except (ImportError,NameError,NotImplementedError):
+#     from time import monotonic_ns as _monotonic_ns  # assume monotonic_ns() exists else we are lame
+#     def ticks_ms(): return _monotonic_ns() // 1_000_000  # stolen from adafruit_ticks
+
+
+###gate_default = 8  # == 50%  (ranges 0-15)
+
+class Step:
+    note = 0
+    vel = 0
+    gate = 1  # ranges from 1-16
+    on = True
+    # def __init__(self, note,vel=127,on=True):
+    #     self.note = note
+    #     self.vel = vel
+    #     self.gate = 0-1? or 0-15? 
+    #     self.on = on
+
+class StepSequencer:
+    def __init__(self, step_count, tempo, playfunc):  # also send callbacks?
+        self.steps_per_beat = 4  # 16th note
+        self.step_count = step_count
+        #self.last_step = last_step   #  || step_count
+        self.i = 0
+        self.steps = [ (0,0,8,True) ] * step_count  # step "object" is tuple (note, vel, gate, on)
+        #self.steps = [ Step(0) for i in range(step_count) ]
+        self.playfunc = playfunc
+        self.set_tempo(tempo)
+        self.last_beat_millis = ticks_ms()
+        self.next_gate_millis = 0
+        self.transpose = 0
+        self.playing = True
+
+    def set_tempo(self,tempo):
+        self.beat_millis = 60_000 // self.steps_per_beat // tempo
+        print("seq.set_tempo: %6d %d" % (self.beat_millis, tempo) )
+        
+    def update(self):
+        if not self.playing: return
+        now = ticks_ms()
+        if now - self.last_beat_millis > self.beat_millis:
+            self.last_beat_millis = now
+            self.i = (self.i + 1) % self.step_count
+            (note,vel,gate,on) = self.steps[self.i]
+            self.next_gate_millis = now + ((self.beat_millis * gate) // 16)  # gate ranges from 1-16
+            self.playfunc(self.i, note, vel, gate, on)
+            
+        if now > self.next_gate_millis and self.next_gate_millis != 0:
+            self.next_gate_millis = 0
+            print("----gate off")
+            
+
+    def stop(self):  # FIXME: what about pending note
+        print("stop!")
+        self.playing = False
+        self.i = 0
+        self.last_beat_millis = 0
+
+   
+    def notenum_to_name(self,notenum):
+        octave = notenum // 12 - 1;
+        n = notenum % 12
+        note_names = ("C","C#","D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+        return note_names[n] + str(octave)
+
+
+
+# if __name__ == "__main__":
+#     import time, random
+    
+#     print("hello there")
+
+#     def play_note(step, note, vel):
+#         print("play: s:%d n:%3d v:%3d" % (step, note,vel) )
+        
+#     seq = StepSequencer(8, 120, play_note)
+
+#     last_time = time.monotonic()
+#     while True:
+#         seq.update()
+        
+#         if time.monotonic() - last_time > 5:
+#             last_time = time.monotonic()
+#             print("hi")
+#             ri = random.randint(0,8)
+#             rn = random.randint(30,50)
+#             rv = random.randint(30,120)
+#             seq[ri] = (rn, rv)
+            
+        
