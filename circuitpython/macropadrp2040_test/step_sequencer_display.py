beaa484ea53924ecb4dcdae85482175968e5ccdf
Tod Kurt <todbot@users.noreply.github.com>
2022-08-09 10:38:01 -0700

    changed to vertical display layout to better fit macropad

---
+++ circuitpython/macropadrp2040_test/step_sequencer_display.py
@@ -0,0 +1,10 @@
+
+import displayio
+
+class StepSequencerDisplay(displayio.Group):
+        def __init__(self):
+            print("here I am!")
+            pass
+        def boop(self):
+            pass
+        
